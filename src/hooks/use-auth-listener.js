2b4de29a0b26cac52d54393014edbdeae9ecd814
Capo Nottoli <andrewnottoli@users-iMac.local>
2020-10-05 23:02:05 -0700

    fix: add error handling, remove build error

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -2,7 +2,9 @@ import { useState, useEffect, useContext } from 'react';
 import { FirebaseContext } from '../context/firebase';
 
 export default function useAuthListener() {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem('authUser'))
+  );
   const { firebase } = useContext(FirebaseContext);
 
   useEffect(() => {
@@ -17,7 +19,7 @@ export default function useAuthListener() {
     });
 
     return () => listener();
-  }, []);
+  }, [firebase]);
 
   return { user };
 }
